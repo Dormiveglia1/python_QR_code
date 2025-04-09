b6e885ae85ef1b63eb952bc5419e406e2b82a53c
Yuhao Zhang(Eric) <ericzhang041227@gmail.com>
2025-04-09 16:52:49 -0400

    change readme in code file

---
+++ code/README.md
@@ -0,0 +1,8 @@
+# python_QR_code/code
+# Introduction to version:
+
+The Version determines the size and complexity of the QR code. 
+
+Version numbers range from 1 to 40, and version 1 is a matrix of 21x21, with each additional version number increasing the size of the matrix by 4 modules (unit cells), so version 40 is a matrix of 177x177. 
+
+The higher the version, the more data the QR code can store, but at the same time it needs more space to display or print.
--- code/create_qr_code.py
+++ code/create_qr_code.py
@@ -1,13 +1,6 @@
 import qrcode
 from PIL import ImageColor
-"""
-The Version determines the size and complexity of the QR code. 
 
-Version numbers range from 1 to 40, and version 1 is a matrix of 21x21, 
-with each additional version number increasing the size of the matrix by 4 modules (unit cells), so version 40 is a matrix of 177x177. 
-
-The higher the version, the more data the QR code can store, but at the same time it needs more space to display or print.
-"""
 def create_qr_code(link, fill='black', back='white', version=1, box_size=10, border=5, error_correction='M', filename='qr.png'):
     # Error correction options
     # Allows QR codes to be read correctly even when partially damaged
